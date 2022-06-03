6fb69b4e120b32d3ec2ffdeae0fdbf6f3d03cbca
mahesh45-py <85050373+mahesh45-py@users.noreply.github.com>
2022-06-03 09:59:37 +0530

    Add files via upload

---
+++ models/Users.py
@@ -0,0 +1,27 @@
+from sqlalchemy import inspect
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    attendance = db.Column(db.Float)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __init__(self,id,name,age,attendance,is_active=True):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.attendance = attendance
+        self.is_active = is_active
+    
+    def __repr__(self):
+
+        return f'{self.name} : {self.id}'
+
+    def toDict(self):
+        return { c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs }
+        
+
