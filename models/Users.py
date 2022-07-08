1ad9bb18924d7cb0d3bc256a41e10be8b002ea12
Mahesh Srikakulam <srikakulammahesh2085@gmail.com>
2022-07-08 16:05:51 +0530

    added new attribute for student object which includes section:string

--- models/Users.py
+++ models/Users.py
@@ -8,13 +8,15 @@ class User(db.Model):
     name = db.Column(db.String)
     age = db.Column(db.Integer)
     attendance = db.Column(db.Float)
+    section = db.Column(db.String)
     is_active = db.Column(db.Boolean, default=True)
 
-    def __init__(self,id,name,age,attendance,is_active=True):
+    def __init__(self,id,name,age,attendance,section,is_active=True):
         self.id = id
         self.name = name
         self.age = age
         self.attendance = attendance
+        self.section = section
         self.is_active = is_active
     
     def __repr__(self):
